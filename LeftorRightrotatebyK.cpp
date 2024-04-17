dd23d3f125d5649b5f764d8bd20379e4594c090e
redcyph <luffyxd79@gmail.com>
2024-04-17 15:23:06 -0700

    rotates the array by k either left or right

---
+++ LeftorRightrotatebyK.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void RotatetoRight(int arr[], int n, int k) {
+  if (n == 0)
+    return;
+  k = k % n;
+  if (k > n)
+    return;
+  int temp[k];
+  for (int i = n - k; i < n; i++) {
+    temp[i - n + k] = arr[i];
+  }
+  for (int i = n - k - 1; i >= 0; i--) {
+    arr[i + k] = arr[i];
+  }
+  for (int i = 0; i < k; i++) {
+    arr[i] = temp[i];
+  }
+}
+
+void RotatetoLeft(int arr[], int n, int k){
+  if (n == 0)
+    return;
+  k = k % n;
+  if (k > n)
+    return;
+  int temp[k];
+  for (int i = 0; i < k; i++) {
+    temp[i] = arr[i];
+  }
+  for (int i = 0; i < n - k; i++) {
+    arr[i] = arr[i + k];
+  }
+  for (int i = n - k; i < n; i++) {
+    arr[i] = temp[i - n + k];
+  }
+}
+
+int main() {
+    int n;
+    int k;
+    char c;
+    cout<<"enter total elements :";
+    cin>>n;
+
+    cout<<"enter which way you want to rotate ? [L/R] :";
+    cin>>c;
+
+    int arr[n];
+    cout<<"enter the elements :"<<endl;
+    for (int i=0; i<n; i++) {
+        cin>>arr[i];
+    }
+
+    cout<<"array before rotation:"<<endl;
+    for (int i=0; i<n; i++) {
+        cout<<arr[i]<<" ";
+    }
+
+
+    cout<<"\nenter the place by you want to rotate :";
+    cin>>k;
+
+    
+
+    if (c=='l' || c=='L') {
+        RotatetoLeft(arr, n, k);
+    }
+
+    else if (c=='r' || c=='R') {
+        RotatetoRight(arr, n, k);
+    }
+    else 
+        cout<<"wrong input";
+
+    cout<<"array after rotation:"<<endl;
+    for (int i=0; i<n; i++) {
+        cout<<arr[i]<<" ";
+    }
+
+
+}
