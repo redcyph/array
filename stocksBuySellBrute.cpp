676d5f91cd5f065d1a748269c4d396c898ff22f3
redcyph <luffyxd79@gmail.com>
2024-11-09 22:34:49 +0530

    added cpp file

---
+++ stocksBuySellBrute.cpp
@@ -0,0 +1,23 @@
+//TC: O(n^2), SC: O(1)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxProfit (vector<int> & arr) {
+    int maxPro = 0;
+    
+    for (int i=0; i<arr.size(); i++) {
+        for (int j=i+1; j<arr.size(); j++) {
+            if(arr[j]>arr[i]) {
+                maxPro = max(arr[j]-arr[i], maxPro);
+            }
+        }
+    }
+    return maxPro;
+}
+
+int main() {
+    vector<int> arr = {7,1,5,3,6,4};
+    int maxPro = maxProfit(arr);
+    cout<<"maximum profit is : "<<maxPro;
+}
